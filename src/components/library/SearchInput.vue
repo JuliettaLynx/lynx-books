f5f2c13dfc306c72063633211b4d96dcac562d3c
Sofya Klimova <sofya.k1imova@yandex.ru>
2026-07-11 18:43:28 +0300

    feat: search icon

--- src/components/library/SearchInput.vue
+++ src/components/library/SearchInput.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="relative w-full">
-    <span
-      class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-base"
-    >
-      🔍
-    </span>
+    <SearchIcon
+      class="absolute left-2 top-1/2 transform -translate-y-1/2 flex w-5 h-5 ml-1 text-border-dark/40 dark:text-border/40"
+    />
     <input
       type="text"
       :value="modelValue"
       autocomplete="off"
       @input="$emit('update:modelValue', $event.target.value)"
       :placeholder="placeholder"
-      class="w-full pl-10 pr-4 py-1.5 border border-border dark:border-border-dark bg-bg-secondary dark:bg-bg-secondary-dark font-light text-bg-primary-dark dark:text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent transition-colors duration-200 placeholder:text-bg-primary-dark/40 dark:placeholder:text-white/40"
+      class="w-full pl-10 pr-4 py-1.5 border border-border dark:border-border-dark bg-bg-secondary dark:bg-bg-secondary-dark font-light text-bg-primary-dark dark:text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent transition-colors duration-200 placeholder:text-border-dark/40 dark:placeholder:text-border/40"
     />
     <button
       v-if="modelValue"
       @click="$emit('update:modelValue', '')"
-      class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+      class="absolute right-3 top-1/2 transform -translate-y-1/2 text-border-dark/40 dark:text-border/40"
     >
       ✕
     </button>
@@ -24,6 +22,8 @@
 </template>
 
 <script setup>
+import SearchIcon from "../../assets/icons/search.svg?component";
+
 defineProps({
   modelValue: {
     type: String,
